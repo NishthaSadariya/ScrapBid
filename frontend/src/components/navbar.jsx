cbed7362d736b5b2bdf6e129dcd01e8f7372c89e
Nistha <2303051057068@paruluniversity.ac.in>
2025-02-11 11:06:08 +0530

    updated

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,16 +1,35 @@
 import React from "react";
+import '../styles/navbar.css'
 
 const Navbar=()=>{
     return(
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 20px', backgroundColor:'#012b5c',color: '#fff', height:'80px'}}>
+        <div className="main-container">
            {/* logo */}
             <strong>ScrapBid</strong>
+
             {/* other links */}
-            <div>
-                <a href="/" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Home</a>
-                <a href="/products" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Products</a>
-                <a href="/profile" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Profile</a>
-                <a href="/logout" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Logout</a>
+            <div className="other-links">
+                <a href="/" className="home">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-house-door">
+                        <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
+                    </svg>
+                </a>
+
+                <a href="/products" >Products</a>
+
+                <a href="/profile" className="profile">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-circle">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                    </svg>
+                </a>
+
+                <a href="/logout" className="logout">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-box-arrow-right">
+                        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+                        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                    </svg>
+                </a>
             </div>
         </div>
     );
