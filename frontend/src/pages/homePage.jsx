8b4def9d1f9c0e2556c1c158cc39d7a65039cc53
Nistha <2303051057068@paruluniversity.ac.in>
2025-02-13 16:18:55 +0530

    updated

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import Header from "../components/header";
 import Navbar from "../components/navbar";
 import "../styles/homePage.css";
+import { Gavel, Recycle, Truck, CreditCard } from "lucide-react";
+import {FaGavel} from "react-icons/fa"
 
-const HomePage=()=>{
-    return(
-        <div>
-            <Header />
-            <Navbar />
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
 
-            {/* Image Section */}
-            <div className="image-container">
-                <img 
-                    src="/Images/ScrapBidHome.jpg" 
-                    alt="Background" 
-                />
-            </div>
+      {/* Image Section */}
+      <div className="image-container">
+        <img src="/Images/ScrapBidHome.jpg" alt="Background" />
+      </div>
 
-            {/* text on image */}
-            {/* <div className="text-overlay">
+      {/* text on image */}
+      {/* <div className="text-overlay">
                 <h1>
                     {"Empowering Sustainability Through E-Waste Auctions".split("").map((char, index) => (
                         <span key={index} style={{ animationDelay: `${index * 0.05}s` }}>
@@ -29,12 +28,62 @@ const HomePage=()=>{
                 <p>Join us in reducing waste and giving products a second life.</p>
             </div> */}
 
-            {/* after image section*/}
-            <div style={{ display: 'flex', backgroundColor: '#51a0fc', height: '150px' }}>
-                
+      {/* after image section*/}
+      <div className="blue-flex-container">
+        <div className="inside-flex-items-main">
+          {/* E-waste */}
+          <div className="items">
+            <div className="icon">
+              <Recycle size={40} />
             </div>
-        </div> 
-    );
-}
+            <h3>Recycle Your E-Waste</h3>
+            <p>Join us to give your e-waste a new life.</p>
+          </div>
+        </div>
+
+        <div className="inside-flex-items-main">
+          {/* Auction */}
+          <div className="items">
+            <div className="icon">
+              <Gavel size={40}/>
+            </div>
+            <h3>Live Scrap Auctions</h3>
+            <p>Bid on scrap materials easily.</p>
+          </div>
+        </div>
+
+        <div className="inside-flex-items-main">
+          {/* Pickup */}
+          <div className="items">
+            <div className="icon">
+              <Truck size={40} />
+            </div>
+            <h3>Hassle-Free Pickup</h3>
+            <p>We arrange scrap pickup at your convenience.</p>
+          </div>
+        </div>
+
+        <div className="inside-flex-items-main">
+          {/* Payment */}
+          <div className="items">
+            <div className="icon">
+              <CreditCard size={40} />
+            </div>
+            <h3>Secure Payments</h3>
+            <p>Safe transactions for buyers & sellers.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* label for featured products */}
+      <div className="Hbid">
+        <p>Highest Bid Products</p>
+        <div className="Hbidicon">
+            <FaGavel size={40} color="#54a0c9"/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
