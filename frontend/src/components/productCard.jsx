de8f8258292bc9c329489858d8488a64824026d9
Nistha <2303051057068@paruluniversity.ac.in>
2025-02-21 18:06:43 +0530

    product card updated

--- frontend/src/components/productCard.jsx
+++ frontend/src/components/productCard.jsx
@@ -1,20 +1,28 @@
 // import {Link} from "react-router-dom";
-import "../styles/productCard.css"
+import React from "react";
+import "../styles/productCard.css";
+import { FaGavel, FaRegHeart } from "react-icons/fa";
 
-const ProductCard =({product})=>{
-    return(
-        <div className="product-card-container">
-            <div className="product-card">
-                <div className="product-img">
-                    <img src= {product.image} alt={product.name}/> 
-                </div>
-                <div className="product-info">
-                    <h3>{product.name}</h3>
-                    <p>Starting Bid: <strong>{product.price}</strong></p>
-                </div>
-            </div>
+const ProductCard = ({ product }) => {
+  return (
+    <div className="product-card-container">
+      <div className="product-card">
+        <div className="product-img">
+          <img src={product.image} alt={product.name} />
+          <div className="product-icons">
+            <FaGavel className="auction-icon" />
+            <FaRegHeart className="like-icon"/>
+          </div>
         </div>
-    );
+        <div className="product-info">
+          <h3>{product.name}</h3>
+          <p>
+            Starting Bid: <strong>{product.price}</strong>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
